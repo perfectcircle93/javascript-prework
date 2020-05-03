e8c6c1858059dcfe09ab0b819c96ebb7bdfcf0d8
Magdalena Świderska <m.swiderska@outlook.com>
2020-05-03 21:24:52 +0200

    add function getmovename

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
+function getMoveName(randomNumber){
+  if(randomNumber == 1){
+    return 'kamień';
+} else if(randomNumber == 2){
+    return 'papier';
+} else if(randomNumber == 3){
+    return 'nożyce';
+} else {
+  printMessage('Nie znam ruchu o id ' + randomNumber + '.');
+  return 'nieznany ruch';
+}
+}
+
 //Computer pick
 
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
-if(randomNumber == 1){
+let computerMove = getMoveName(randomNumber);
+
+/*if(randomNumber == 1){
   computerMove = 'kamień';
 } else if(randomNumber == 2){
   computerMove = 'papier';
-} else if(randomNumber == 2){
+} else if(randomNumber == 3){
   computerMove = 'nożyce';
-}
+}*/
 
 printMessage('Mój ruch to: ' + computerMove);
 
@@ -20,15 +35,15 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(randomNumber);
 
-if(playerInput == '1'){
+/*if(playerInput == '1'){
   playerMove = 'kamień';
 } else if(playerInput == '2'){
   playerMove = 'papier';
 } else if(playerInput == '3'){
   playerMove = 'nożyce';
-}
+}*/
 
 printMessage('Twój ruch to: ' + playerMove);
 
