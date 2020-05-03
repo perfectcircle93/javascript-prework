b1fd9c4b1e57dc3241ec7e765453e761d6d7c599
Magdalena Świderska <m.swiderska@outlook.com>
2020-05-03 12:05:24 +0200

    Add computerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-let playerMove = `papier`;
+//computer pick
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+
+else if(randomNumber == 2){
+  computerMove = 'papier';
+}
+
+else if(randomNumber == 2){
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
