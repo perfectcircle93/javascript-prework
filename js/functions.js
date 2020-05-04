d2fddb431ae6fe06fe29fb6f56aa7080cec61b74
Magdalena Świderska <m.swiderska@outlook.com>
2020-05-04 15:26:03 +0200

    change js/functions

--- js/functions.js
+++ js/functions.js
@@ -7,32 +7,3 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-
-function printMessageRed(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages-red').appendChild(div);
-}
-
-function clearMessagesRed(){
-	document.getElementById('messages-red').innerHTML = '';
-}
-
-function printMessageGreen(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages-green').appendChild(div);
-}
-
-function clearMessagesGreen(){
-	document.getElementById('messages-green').innerHTML = '';
-}
-function printMessageWin(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages-win').appendChild(div);
-}
-
-function clearMessagesWin(){
-	document.getElementById('messages-win').innerHTML = '';
-}
